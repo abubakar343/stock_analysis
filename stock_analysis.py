ca539f07b0251bc21d1251a1b58d33a0eb2e4c25
abubakar343 <100688909+abubakar343@users.noreply.github.com>
2024-08-19 13:35:02 +0500

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 #importing necessary libraries
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,7 +9,6 @@ import seaborn as sns
 from pandas_datareader import data as wb
 
 #insert the ticker, set the duration and fetch prices
-
 stock_tickers = ["F","TSLA"]
 stock_prices = wb.get_data_yahoo(stock_tickers,start = "2021-05-01",end = "2022-05-07")
 print(stock_prices.head())
@@ -22,18 +20,15 @@ fig = plt.figure()
 plt.show()
 
 #monthly returns
-
 stock_monthly_returns = stock_prices['Adj Close'].resample('M').ffill().fillna(0).pct_change()
 print(stock_monthly_returns)
 
 #mean, standard deviation and variance of monthly returns
-
 print(stock_monthly_returns.mean())
 print(stock_monthly_returns.std())
 print(stock_monthly_returns.var())
 
 #correlation and co-variance
-
 print(stock_monthly_returns.corr())
 print(stock_monthly_returns.cov())
 
